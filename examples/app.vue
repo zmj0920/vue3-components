f0f82bc62f1c33b54c3c7a5965fd5266d0fcd566
mengjie.zhang <mengjie.zhang@easystack.cn>
2022-12-30 15:09:18 +0800

    初始化组件库

---
+++ examples/app.vue
@@ -0,0 +1,9 @@
+<template>
+    <div>
+        <Button />
+    </div>
+</template>
+
+<script lang="ts" setup>
+import { Button } from 'vue-view-ui'
+</script>
---
+++ packages/components/src/index.ts
@@ -0,0 +1,4 @@
+import { default as Button } from "./button";
+import { default as Icon } from "./icon";
+export { Button, Icon };
+export default [Button, Icon];
